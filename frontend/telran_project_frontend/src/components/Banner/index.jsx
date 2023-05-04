5dc1045746d0fbc165ed64ac9ffb552b654856cd
miaraylight <miaraylight@gmail.com>
2023-05-04 20:40:41 +0200

    changed design and rewrote banner and header

--- frontend/telran_project_frontend/src/components/Banner/index.jsx
+++ frontend/telran_project_frontend/src/components/Banner/index.jsx
@@ -5,13 +5,18 @@ import { Link } from 'react-router-dom'
 export default function Banner() {
   const link = '/product/sale'
   return (
-    <div className={s.banner}>
-        <h3>Sale</h3>
-        <p>New season</p>
-        <Link to={link}>
-          <button>Sale</button>
-        </Link>
-        
-  </div>
+    <div className={s.container}>
+      <div className={s.banner}>
+        <div className={s.banner_content}>
+          <h3>SaleW</h3>
+          <p>New season</p>
+          <Link to={link}>
+            <button>Shop now</button></Link>
+        </div>
+        <div className={s.banner_img}>
+          <img src={process.env.PUBLIC_URL + '/images/banner-background.png'} alt="banner-background" />
+        </div>
+      </div>
+    </div>
   )
 }
--- frontend/telran_project_frontend/src/components/Header/index.jsx
+++ frontend/telran_project_frontend/src/components/Header/index.jsx
@@ -5,16 +5,18 @@ import s from './style.module.css'
 export default function Header() {
   return (
     <div className={s.container}>
-      <div className={s.logo_wrapper}>
-        <img src={process.env.PUBLIC_URL + '/images/logo_icon.svg'} alt="" />
-        <NavLink className={s.catalog} to='/categories'>Catalog</NavLink>
-      </div>
+      <NavLink className={s.logo_link} to='/'>eGrow <span>plants</span></NavLink>
       <div className={s.links_wrapper}>
-        <NavLink className={s.link} to='/'>Main Page</NavLink>
-        <NavLink className={s.link} to='/product/all'>All products</NavLink>
-        <NavLink className={s.link} to='/product/sale'>All sales</NavLink>
+        <NavLink className={s.link} to='/categories'>Catalog</NavLink>
+        <NavLink className={s.link} to='/product/all'>Shop all</NavLink>
+        <NavLink className={s.link} to='/product/sale'>On sale</NavLink>
+      </div>
+
+      <div className={s.links_wrapper}>
+        <NavLink className={s.link} to='/'>Home</NavLink>
+        <NavLink>Contact</NavLink>
         <NavLink className={s.link_basket} to='/basket'>
-          <img src={process.env.PUBLIC_URL + '/images/basket_icon.svg'} alt="" />
+          <img src={process.env.PUBLIC_URL + '/images/basket-icon.png'} alt="basket-icon" /> 
         </NavLink>
       </div>
     </div>
