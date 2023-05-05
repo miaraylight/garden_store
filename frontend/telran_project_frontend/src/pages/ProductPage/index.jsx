a54b98b80d01bbb9715c91a18a3bf87a77b270c2
miaraylight <miaraylight@gmail.com>
2023-05-05 20:30:51 +0200

    added header text output depending on the context on ProductPage

--- frontend/telran_project_frontend/src/pages/ProductPage/index.jsx
+++ frontend/telran_project_frontend/src/pages/ProductPage/index.jsx
@@ -7,12 +7,17 @@ import s from './style.module.css'
 export default function ProductPage() {
   const { category, sale } = useParams()
 
+  let header;
+
   const products = useSelector(state => {
     if (sale !== undefined) {
+      header = "Sale"
       return state.product.list.filter(({discont_price}) => discont_price !== null)  
     }else if (category !== undefined) {
+      header = state.category.list.find(({id}) => +id === +category).title
       return state.product.list.filter(({categoryId})=> +categoryId === +category)
     }
+    header = "All products"
     return state.product.list
   }
   )
@@ -20,9 +25,13 @@ export default function ProductPage() {
 
   return (
     <div className={s.container}>
-      {
-        products.map(item => <ProductItem key={item.id} {...item}/>)
-      }
+      <h1>{header}</h1>
+      <div className={s.product_wrapper}>
+        {
+          products.map(item => <ProductItem key={item.id} {...item}/>)
+        }
+      </div>
+      
     </div>
   )
 }
