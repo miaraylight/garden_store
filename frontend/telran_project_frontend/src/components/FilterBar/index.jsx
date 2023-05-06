ceb7f57e03d4af48e925bbd0e49d967eece8d3e1
miaraylight <miaraylight@gmail.com>
2023-05-06 19:05:55 +0200

    added markup and styles to FilterBar component

--- frontend/telran_project_frontend/src/components/FilterBar/index.jsx
+++ frontend/telran_project_frontend/src/components/FilterBar/index.jsx
@@ -1,28 +1,48 @@
 import React from 'react'
-import s from './style.module.css'
+// import s from './style.module.css'
 import ReactSlider from 'react-slider'
-
+import './filterbar.css'
 
 export default function FilterBar() {
   return (
-    <div className={s.container}>
-          <div className={s.filter}>
+    <div className="container">
+          <div className="toolbar">
+            <div className='filter-block'>
+              <button className="filter-btn">
+                <img src={process.env.PUBLIC_URL + '/images/filter-icon.png'} alt="filter-icon" />
+              </button>
+              <span>Filter by price</span>
+            </div>
             <ReactSlider
-                className={s.horizontal_slider}
-                thumbClassName={s.example_thumb}
-                trackClassName={s.example_track}
+                className="horizontal_slider"
+                thumbClassName="example_thumb"
+                trackClassName="example-track"
                 defaultValue={[0, 100]}
                 ariaLabel={['Lower thumb', 'Upper thumb']}
                 ariaValuetext={state => `Thumb value ${state.valueNow}`}
                 renderThumb={(props, state) => <div {...props}>{state.valueNow}</div>}
                 pearling
                 minDistance={10}
-            />  
-            <select name="filter_options" className={s.filter_options}>
-                <option value="default">default</option>
+            /> 
+            <div className='discount-block'>
+              <input type="checkbox" name="discounted"/>
+              <label htmlFor="discounted"> Discounted</label>
+            </div> 
+            <div className="sort_block">
+              <select name="sort_options" className='select'>
+                <option value="default">Default sorting</option>
                 <option value="lowToHigh">Sort by price: low to high</option>
                 <option value="highToLow">Sort by price: high to low</option>
-            </select>
+              </select>
+              <button className="grid-btn">
+                <img src={process.env.PUBLIC_URL + '/images/grid-view.png'} alt="grid-view-icon" />
+              </button>
+              <button className="list-btn">
+                <img src={process.env.PUBLIC_URL + '/images/list-view.png'} alt="list-view-icon" />
+              </button>
+              
+            </div>
+            
       </div>
     </div>
   )
