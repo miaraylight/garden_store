ba8ee19019e6b1f1b74ae0017d6f016e42ada911
miaraylight <miaraylight@gmail.com>
2023-05-13 18:54:21 +0200

    rewrote ProductDescriptionPage according new layout

--- frontend/telran_project_frontend/src/pages/ProductDescriptionPage/index.jsx
+++ frontend/telran_project_frontend/src/pages/ProductDescriptionPage/index.jsx
@@ -5,30 +5,60 @@ import s from './style.module.css'
 
 export default function ProductDescriptionPage() {
     const { productId } = useParams()
-    const product = useSelector(state => state.product.list.find(({id}) => id === +productId))
-    const { id, title, price, discont_price, image, description } = product;
+    const product = useSelector(state => state.product.list.find(({id}) => +id === +productId))
+    const {id, title, price, discont_price, final_price, image, description } = product;
   return (
     <div className={s.container}>
-        <h1>{title}</h1>
         <div className={s.content}>
-            <div className={s.img_wrapper}>
-                <img src={`http://localhost:3333/${image}`} alt="product" />
-            </div>
-            <div className={s.info_wrapper}>
-                {
-                discont_price === null 
-                ? <div className={s.card_price}>
-                    <p className={price}>{price}</p>
+            <div className={s.product_item}>
+               <div className={s.product_item_img}>
+                    <img src={`http://localhost:3333/${image}`} alt="product" />
+                </div>
+                <div className={s.product_item_info}>
+                    <h1>{title}</h1>
+                    <div className={s.product_item_price_block}>
+                        {
+                        discont_price === null 
+                        ? <div className={s.product_item_price}>
+                            <p className={s.final_price}>{final_price}</p>
+                            </div>
+                        : <div className={s.product_item_price}>
+                            <p className={s.throughed_price}>${price}</p>
+                            <p className={s.final_price}>${final_price}</p>
+                            <p className={s.discount_percentage}>-{((price - discont_price) * 100 / price).toFixed(1)}%</p>
+                            </div>
+                        }
+                        <div className={s.product_item_toolbar}>
+                            <div className={s.product_item_countbar}>
+                                <button >-</button>
+                                <p>1</p>
+                                <button >+ </button>
+                            </div>
+                           <button className={s.product_item_addBtn}>Add to cart</button> 
+                        </div>
+                        <div className={s.product_item_payment}>
+                            <p>Guaranteed safe checkout</p>
+                            <div className={s.product_item_payment_img}>
+                                <img src={process.env.PUBLIC_URL + '/images/visacard-icon.png'} alt="" />
+                                <img src={process.env.PUBLIC_URL + '/images/mastercard-icon.png'} alt="" />
+                                <img className={s.icon} src={process.env.PUBLIC_URL + '/images/american-express-icon.png'} alt="" />
+                                <img className={s.icon}src={process.env.PUBLIC_URL + '/images/discover-icon.png'} alt="" />
+                            </div>
+                        </div>
+                       <div className={s.product_item_payment_info}>
+                        <p><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-1.25 16.518l-4.5-4.319 1.396-1.435 3.078 2.937 6.105-6.218 1.421 1.409-7.5 7.626z"/></svg>No-Risk Money Back Qurantee</p>
+                        <p><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-1.25 16.518l-4.5-4.319 1.396-1.435 3.078 2.937 6.105-6.218 1.421 1.409-7.5 7.626z"/></svg>No Hassie Refunds</p>
+                        <p><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-1.25 16.518l-4.5-4.319 1.396-1.435 3.078 2.937 6.105-6.218 1.421 1.409-7.5 7.626z"/></svg>Secure Payments</p>
+                       </div>
                     </div>
-                : <div className={s.card_price}>
-                    <p className={s.dicount_price}>{discont_price}<span>$</span></p>
-                    <p className={s.throughed_price}>{price}$</p>
-                    <p className={s.discount_percentage}>-{((price - discont_price) * 100 / price).toFixed(1)}<span>%</span></p>
-                    </div>
-                }
-                <button className={s.add_button}>Add</button>
-                <p className={s.descr}><span className={s.descr_span}>Description:</span>{description}</p>
+
+                </div> 
             </div>
+            <div className={s.product_descr}>
+                <span className={s.descr_span}>Description</span>
+                <p className={s.descr}>{description}</p>
+            </div>
+                        
         </div>
     </div>
   )
