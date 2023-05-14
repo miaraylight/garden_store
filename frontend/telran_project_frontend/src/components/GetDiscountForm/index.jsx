a22563cdd3b9da6ea98a7ddac53064fbf09d4239
miaraylight <miaraylight@gmail.com>
2023-05-14 14:35:03 +0200

    installed useForm and start work with Banner's form

--- frontend/telran_project_frontend/src/components/GetDiscountForm/index.jsx
+++ frontend/telran_project_frontend/src/components/GetDiscountForm/index.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import s from './style.module.css'
 
 export default function GetDiscountForm() {
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault()
+  }
   return (
     <div className={s.container}>
       <div className={s.content_wrapper}>
         <div className={s.form_wrapper}>
           <h3>5% off on the first order</h3>
-          <form className={s.discount_form}>
-            <input type="text" placeholder='+49' />
+          <form className={s.discount_form} onSubmit={(e)=>onSubmitHandler(e)}>
+            <input type="number" placeholder='+49' required/>
             <button>Get a discount</button>
           </form>
         </div>
--- frontend/telran_project_frontend/src/components/ProductItem/index.jsx
+++ frontend/telran_project_frontend/src/components/ProductItem/index.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import s from "./style.module.css";
 import { Link } from "react-router-dom";
 import { addItemToBasket } from "../../store/slice/basketSlice";
-import { useDispatch } from 'react-redux'
+import { useDispatch } from "react-redux";
 
 export default function ProductItem({
   id,
@@ -11,80 +11,79 @@ export default function ProductItem({
   discont_price,
   image,
 }) {
-  
   const link = `/product/item/${id}`;
   const [toggleBasketBtn, setToggleBasketBtn] = useState({ display: "none" });
   const [togglePopUp, setTogglePopUp] = useState({ display: "none" });
-  const dispatch = useDispatch()
-  
+  const dispatch = useDispatch();
+
   return (
-    <div className={s.product_card}>
-      <div 
-      className={s.card_img_block}
+    
+    <div
+      className={s.product_card}
       onMouseEnter={(e) => {
         setToggleBasketBtn({ display: "flex" });
       }}
       onMouseLeave={(e) => {
-        setToggleBasketBtn({display: "none"})
+        setToggleBasketBtn({ display: "none" });
       }}
-      >
-        <div className={s.card_top}>
-          <span
-            className={s.discount_percentage}
-            style={
-              discont_price === null
-                ? { display: "none" }
-                : { display: "block" }
-            }
-          >
-            -{(((price - discont_price) * 100) / price).toFixed(1)}%
-          </span>
-          <div className={s.card_toolbar} >
-            <button 
-            className={s.basket}
-            onMouseEnter={(e) => {
-              setTogglePopUp({ display: "flex" });
-            }}
-            onMouseLeave={(e) => {
-              setTogglePopUp({display: "none"})
-            }}
+    >
+      <div className={s.card_top}>
+            <span
+              className={s.discount_percentage}
+              style={
+                discont_price === null
+                  ? { display: "none" }
+                  : { display: "block" }
+              }
             >
-              <span className={s.basket_pop_up} 
-              style={togglePopUp}
-              >Add to cart</span>
-              <div 
-              className={s.basket_btn} 
-              style={toggleBasketBtn}
-              onClick={()=>dispatch(addItemToBasket({product_id: id}))}
+              -{(((price - discont_price) * 100) / price).toFixed(1)}%
+            </span>
+            <div className={s.card_toolbar}>
+              <button
+                className={s.basket}
+                
+                onMouseEnter={(e) => {
+                  setTogglePopUp({ display: "flex" });
+                }}
+                onMouseLeave={(e) => {
+                  setTogglePopUp({ display: "none" });
+                }}
               >
-                <img 
-                  src={process.env.PUBLIC_URL + "/images/basket-icon.png"}
-                  alt="basket-icon"
-              />
-              </div>
-              
-            </button>
+                <span className={s.basket_pop_up} style={togglePopUp}>
+                  Add to cart
+                </span>
+                <div
+                  className={s.basket_btn}
+                  style={toggleBasketBtn}
+                  onClick={() => {
+                    dispatch(addItemToBasket({ product_id: id }))
+                  }}
+                >
+                  <img
+                    src={process.env.PUBLIC_URL + "/images/basket-icon.png"}
+                    alt="basket-icon"
+                  />
+                </div>
+              </button>
+            </div>
+          </div>
+      <Link to={link}>
+        <div className={s.card_img_block}>
+          
+          <div className={s.card_img}>
+            <img src={`http://localhost:3333/${image}`} alt="product" />
           </div>
         </div>
-        <div className={s.card_img} >
-          <img
-          src={`http://localhost:3333/${image}`}
-          alt="product"
-        />
-        </div>
-        
-      </div>
 
-      <Link to={link}>
         <div className={s.card_content_block}>
           <p className={s.card_title}>{title}</p>
           <div className={s.card_price_block}>
             <p className={s.price}>${price}</p>
             <p className={s.dicount_price}>${discont_price}</p>
-            
           </div>
         </div>
       </Link>
     </div>
+    
   );
 }
